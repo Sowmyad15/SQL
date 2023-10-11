a2c1c43103d04acf74b849c8c1878c951cbe8c5f
Sowmya D <88073238+Sowmyad15@users.noreply.github.com>
2023-10-11 21:20:26 +0530

    Time: 122 ms (51.56%), Space: 14.9 MB (62.6%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d={}
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+            if d[i]>len(nums)/2:
+                return i
+        
